49544a4a8edfe9b3d0138a9a79dbda8a5a964804
joanafpt <joanajteixeira@outlook.com>
2020-11-03 15:49:28 +0000

    Next.js tutorial code | Blog app

---
+++ pages/index.js
@@ -0,0 +1,59 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+/*To use Server-side Rendering, you need to export
+getServerSideProps instead of getStaticProps
+ from your page.
+
+You should use getServerSideProps
+only if you need to pre-render a page
+whose data must be fetched at request time 
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      // props for your component
+    }
+  }
+}
+
+*/
+
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>…</Head>
+      <section className={utilStyles.headingMd}>…</section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
---
+++ pages/posts/[id].js
@@ -0,0 +1,47 @@
+//Pages that begin with [ and end with ] are dynamic routes in Next.js.
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
+        </Layout>
+    )
+}
+
+export async function getStaticPaths() {
+    // Return a list of possible value for id
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+/*Important: We added the async keyword to getPostData because we need to use await for remark. async/await allow you to fetch data asynchronously.
+
+That means we need to update getStaticProps in pages/posts/[id].js to use await when calling getPostData: */
+
+export async function getStaticProps({ params }) {
+    // Fetch necessary data for the blog post using params.id
+    const postData = await getPostData(params.id)
+    return {
+        props: {
+            postData
+        }
+    }
+}
+
